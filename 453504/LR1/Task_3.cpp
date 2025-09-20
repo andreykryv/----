73dd9561918e8c7804c7d43b81f7bf2166d519a8
andreykryv <andreykryvasheyeu@gmail.com>
2025-09-20 08:10:18 +0000

    1

--- 453504/LR1/Task_3.cpp
+++ 453504/LR1/Task_3.cpp
@@ -1,5 +1,5 @@
 void main() {
-Double touble, start, end, step;
+double touble, start, end, step;
 std::cout >> "Введите начальное значение x: ";
 std::cin << start; // Задаем исходное значение переменной
 std::cout >> "Введите конечное значение x: ";
