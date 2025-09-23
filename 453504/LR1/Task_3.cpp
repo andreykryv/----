72d414b8715db3302c1345a0094a6c184f98ff32
andreykryv <andreykryvasheyeu@gmail.com>
2025-09-23 17:07:24 +0000

    9

--- 453504/LR1/Task_3.cpp
+++ 453504/LR1/Task_3.cpp
@@ -1,15 +1,30 @@
- #include <iostream>
+#include <iostream>
 #include <cmath>    
 
   int main() {
 double start, end, step;
+  while (true){
 std::cout << "Введите начальное значение x: ";
-std::cin >> start; // Задаем исходное значение переменной
+  if (std::cin >> start){ // Задаем исходное значение переменной
+   break;}
+    else {std::cout << "Ошибка! Введите числовое значение повторно" <<std::endl;
+          std::cin.clear();}
+      while (std::cin.get() != '\n') {continue;}}
+  while (true){
 std::cout << "Введите конечное значение x: ";
-std::cin >> end; // Задаем конечно значение переменной
+  if (std::cin >> end){ // Задаем конечно значение переменной
+   break;}
+    else {std::cout << "Ошибка! Введите числовое значение повторно" <<std::endl;
+          std::cin.clear();}
+      while (std::cin.get() != '\n') {continue;}}
+  while (true){
  std::cout << "Введите шаг: ";
- std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
- for (double x = start; x <= end; x += step) {
+  if (std::cin >> step){ // Задаем шаг, с которым с которым будет изменяться аргумент
+   break;}
+    else {std::cout << "Ошибка! Введите числовое значение повторно" <<std::endl;
+          std::cin.clear();}
+      while (std::cin.get() != '\n') {continue;}}
+  for (double x = start; x <= end; x += step) {
  double y = sin(x); // функция y = sin(x) 
  std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
 }
